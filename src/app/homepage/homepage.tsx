bdd0d9864642ec72eba846f4d8f542bd97bbccdb
watcharapol <watcharapol.numpaya@gtnexttech.com>
2024-08-16 11:26:21 +0700

    [UPDATE] our service

--- src/app/homepage/homepage.tsx
+++ src/app/homepage/homepage.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import PartnerCarousel from "./components/PartnerCarousel";
- 
+
 export default function Homepage() {
- 
   const renderHomepage = () => {
     return (
       <div className="xl:container mx-auto">
@@ -11,14 +10,17 @@ export default function Homepage() {
         <p className="text-center text-3xl">PROUD PARTNER TO GREAT BRANDS</p>
         <div id="partner-carousel" className="bg-orange-400  ">
           <PartnerCarousel />
-        </div> 
-        <div id="our-service" className="bg-gray-400 h-96" >
-          <p className="text-center text-3xl py-10 font-bold bg-red-400">OUR SERVICES</p>
-          
+        </div>
+        <div id="our-service" className="bg-red-400 h-96">
+          <div className="bg-green-400 h-1/5 flex justify-center items-center">
+            <p className="text-center text-3xl  font-bold   ">OUR SERVICES</p>
+          </div>
+          <div className="bg-green-800 h-2/5"></div>
+          <div className="bg-yellow-400 h-2/5"></div>
         </div>
       </div>
     );
   };
 
-  return <div className="flex-1">{renderHomepage()}</div>;
+  return <div className="flex-1  ">{renderHomepage()}</div>;
 }
