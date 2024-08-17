4bb6547d74ed3114c9c47319459167322d557b96
watcharapol <watcharapol.numpaya@gtnexttech.com>
2024-08-18 01:38:54 +0700

    [UPDATE] our service custom space

--- src/app/homepage/homepage.tsx
+++ src/app/homepage/homepage.tsx
@@ -8,23 +8,23 @@ import TechnologyExpertiseSection from "./components/technology-expertise-sectio
 export default function Homepage() {
   const renderOurService = () => {
     return (
-      <div id="our-service" className="h-144 relative ">
-        <div className="absolute inset-0 py-2 grid grid-rows-[auto,1fr,auto] items-center justify-items-center">
+      <div id="our-service" className=" h-218 md:h-160 lg:h-128 relative ">
+        <div className="absolute p-2 inset-0 py-4 md:py-6 grid grid-rows-[auto,1fr,auto] items-center justify-items-center">
           <p className="text-3xl font-bold">OUR SERVICES</p>
 
-          <div className="grid grid-cols-3 lg:grid-cols-6 w-full justify-items-center items-center gap-2">
-            <div className="h-60 w-40 bg-red-400 rounded-xl"> </div>
-            <div className="h-60 w-40 bg-red-400 rounded-xl"> </div>
-            <div className="h-60 w-40 bg-red-400 rounded-xl"> </div>
-            <div className="h-60 w-40 bg-red-400 rounded-xl"> </div>
-            <div className="h-60 w-40 bg-red-400 rounded-xl"> </div>
-            <div className="h-60 w-40 bg-red-400 rounded-xl"> </div>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6  w-full justify-items-center items-center gap-2">
+            <div className="h-60 w-40 bg-white rounded-xl"> </div>
+            <div className="h-60 w-40 bg-white rounded-xl"> </div>
+            <div className="h-60 w-40 bg-white rounded-xl"> </div>
+            <div className="h-60 w-40 bg-white rounded-xl"> </div>
+            <div className="h-60 w-40 bg-white rounded-xl"> </div>
+            <div className="h-60 w-40 bg-white rounded-xl"> </div>
           </div>
           <Button title="View All Service" />
         </div>
 
-        <div className="bg-green-800 h-1/2"></div>
-        <div className="bg-yellow-400 h-1/2"></div>
+        <div className="bg-primary h-1/2"></div>
+        <div className="bg-neutral-200 h-1/2"></div>
       </div>
     );
   };
