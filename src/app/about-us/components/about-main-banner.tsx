0e51c94605699b273a7c5b3bbdee56ea4d724220
watcharapol <watcharapol.numpaya@gtnexttech.com>
2024-08-21 13:18:34 +0700

    [UPDATE] add about us text

---
+++ src/app/about-us/components/about-main-banner.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+import meeting_img from "@/app/assets/about-us/millennial-group-young-businesspeople-asia-businessman-businesswoman-celebrate-giving-five-after-dealing-feeling-happy-signing-contract-agreement-meeting-room-small-mod.jpg";
+
+export default function AboutMainBanner() {
+  return (
+    <div className="relative lg:h-152 md:h-96 h-80">
+      <Image
+        alt=""
+        src={meeting_img}
+        className="clip-style-v4 absolute lg:h-152 md:h-96 h-80 object-cover  "
+      />
+      <div className="md:hidden absolute h-full w-full z-20 flex items-center justify-center">
+        <p className="text-white text-5xl pb-20 font-bold drop-shadow-lg">ABOUT US</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import React from "react";
+import meeting_img from "@/app/assets/about-us/millennial-group-young-businesspeople-asia-businessman-businesswoman-celebrate-giving-five-after-dealing-feeling-happy-signing-contract-agreement-meeting-room-small-mod.jpg";
+import AboutMainBanner from "./components/about-main-banner";
+
+export default function AboutUs() {
+  const renderDescription = () => {
+    return (
+      <div className="py-10 px-2 md:text-lg text-base">
+        <p>
+          SEA-Bangkok based leading technology consulting firm with solution
+          development team from Bangalore India. With exceptional team of highly
+          experienced professionals and expertise across multiple industries,
+        </p>
+        <p>
+          we are dedicated to assist clients in optimizing their business
+          operations through the strategic utilization of Information
+          Technology. By understanding the unique needs and challenges of each
+          client, we can tailor our services to deliver custom-made solutions
+          that address specific requirements.
+        </p>
+      </div>
+    );
+  };
+
+  const renderAboutUs = () => {
+    return (
+      <div className="container mx-auto">
+        <AboutMainBanner />
+        {renderDescription()}
+      </div>
+    );
+  };
+
+  return <div className="flex-1">{renderAboutUs()}</div>;
+}
