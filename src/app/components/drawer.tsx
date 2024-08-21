5c3a4b3e6c285e23501b81a1574660095aebe670
watcharapol <watcharapol.numpaya@gtnexttech.com>
2024-08-21 12:24:55 +0700

    [UPDATE] add Drawer

---
+++ src/app/components/drawer.tsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import Link from "next/link";
+
+export default function DrawerComponent() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const link = [
+    "/",
+    "/about-us",
+    "/our-service",
+    "/our-strengths",
+    "/our-teams",
+    "/contact-us",
+  ];
+
+  const DrawerList = (
+    <Box
+      sx={{ width: "auto" }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+    >
+      <List>
+        {[
+          "HOME",
+          "ABOUT US",
+          "OUR SERVICE",
+          "OUR STRENGTHES",
+          "OUR TEAMS",
+          "CONTACT US",
+        ].map((text, index) => (
+          <Link key={text} href={link[index]} passHref>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} sx={{ textAlign: "right" }} />
+              </ListItemButton>
+            </ListItem>{" "}
+          </Link>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div className="w-full h-18 flex items-center bg-primary">
+      <div className="flex justify-between w-full px-2">
+        <div className="h-10 w-10 "> </div>
+        <div className="h-10 w-10 bg-red-400">icon</div>
+        <button onClick={toggleDrawer(true)}>
+          <MenuIcon fontSize="large" />
+        </button>
+      </div>
+
+      <Drawer anchor="top" open={open} onClose={toggleDrawer(false)}>
+        <div className="flex justify-between items-center h-18 w-full px-2 bg-primary ">
+          <div className="h-10 w-10 "> </div>
+          <div className="h-10 w-10 bg-red-400">icon</div>
+          <button onClick={toggleDrawer(false)}>
+            <HighlightOffIcon fontSize="large" className="text-white" />
+          </button>
+        </div>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
