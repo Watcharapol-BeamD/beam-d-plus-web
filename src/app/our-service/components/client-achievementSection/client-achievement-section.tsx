4ef5a4326b37a862e44bdc64021e1d09be2d0f84
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2024-08-22 15:21:21 +0700

    [UPDATE] add bg image

--- src/app/our-service/components/client-achievementSection/client-achievement-section.tsx
+++ src/app/our-service/components/client-achievementSection/client-achievement-section.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import mock_img from "@/app/assets/join.png";
 
 export default function ClientAchievementSection() {
   const renderAchievementCard = () => {
-    return <div className="h-76 w-76 bg-red-400 m-4 rounded-3xl">sss</div>;
+    return (
+      <div className="  bg-red-400 m-4 rounded-3xl">
+        <div
+          className="bg-cover bg-center h-76 w-76  rounded-3xl overflow-hidden text-white"
+          style={{ backgroundImage: `url(${mock_img.src})` }}
+        >
+          sssdsasdas
+        </div>
+        {/* <Image alt="" src={mock_img} /> */}
+      </div>
+    );
   };
 
   return <div>{renderAchievementCard()}</div>;
