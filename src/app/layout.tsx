767b79001ecc9253d28b655515492d8db29c826a
watcharapol <watcharapol.numpaya@gtnexttech.com>
2024-08-23 19:23:29 +0700

    [UPDATE] enable component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className+"bg-red-400"}>
-        {/* <Navbar />
-        {children} */}
+        <Navbar />
+        {children}
         <Footer/>
       </body>
     </html>
