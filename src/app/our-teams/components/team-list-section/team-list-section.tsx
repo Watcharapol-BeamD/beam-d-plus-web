37d68b564bd7267f40e6039c09903b25a90a2dab
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2024-08-26 17:44:02 +0700

    [UPDATE] add team img

--- src/app/our-teams/components/team-list-section/team-list-section.tsx
+++ src/app/our-teams/components/team-list-section/team-list-section.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import kannika_img from "@/app/assets/our-team/teammate/kannika_img.webp";
-import { StaticImageData } from "next/image";
+import Image, { StaticImageData } from "next/image";
 import team_img from "@/app/assets/our-team/african-head-department-is-satisfied-with-work-staff-asian-male-student-trendy-shirt-telling-univers.webp";
 
 type TeamCardProps = {
@@ -15,15 +15,36 @@ export default function TeamListSection() {
       full_name: "Kannika Angkurarat (May)",
       position: "",
     },
+    {
+      image_name: kannika_img,
+      full_name: "Kannika Angkurarat (May)",
+      position: "",
+    },
   ];
 
-  const renderTeamCard =()=>{
-    return <div className="h-40 w-20 bg-white">ssss</div>
-  }
-
-  const renderCardList = () => {
-    return <div>{renderTeamCard()}</div>;
+  const renderTeamCard = (item: TeamCardProps) => {
+    return (
+      <div className="h-full w-56 rounded-tr-3xl rounded-bl-3xl overflow-hidden p-2 border border-primary">
+        <Image
+          alt=""
+          src={item.image_name}
+          className="h-full w-56 rounded-tr-3xl rounded-bl-3xl"
+        />
+      </div>
+    );
   };
 
-  return < >{renderCardList()}</>;
+  const renderCardList = () => {
+    return (
+      <div className="m-4 h-full w-full flex gap-8">
+        {data.map((item: TeamCardProps, index: number) => {
+          return (
+            <React.Fragment key={index}>{renderTeamCard(item)}</React.Fragment>
+          );
+        })}
+      </div>
+    );
+  };
+
+  return <>{renderCardList()}</>;
 }
