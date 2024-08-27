23ce2bc005f326116e47ec5e9a44db6585f2afb4
watcharapol <watcharapol.numpaya@gtnexttech.com>
2024-08-27 11:15:32 +0700

    [UPDATE] adjust flex

--- src/app/our-teams/components/team-list-section/team-list-section.tsx
+++ src/app/our-teams/components/team-list-section/team-list-section.tsx
@@ -20,6 +20,53 @@ export default function TeamListSection() {
       full_name: "Kannika Angkurarat (May)",
       position: "",
     },
+    {
+      image_name: kannika_img,
+      full_name: "Kannika Angkurarat (May)",
+      position: "",
+    },
+    {
+      image_name: kannika_img,
+      full_name: "Kannika Angkurarat (May)",
+      position: "",
+    },
+    {
+      image_name: kannika_img,
+      full_name: "Kannika Angkurarat (May)",
+      position: "",
+    },
+    {
+      image_name: kannika_img,
+      full_name: "Kannika Angkurarat (May)",
+      position: "",
+    },{
+      image_name: kannika_img,
+      full_name: "Kannika Angkurarat (May)",
+      position: "",
+    },
+    {
+      image_name: kannika_img,
+      full_name: "Kannika Angkurarat (May)",
+      position: "",
+    },{
+      image_name: kannika_img,
+      full_name: "Kannika Angkurarat (May)",
+      position: "",
+    },
+    {
+      image_name: kannika_img,
+      full_name: "Kannika Angkurarat (May)",
+      position: "",
+    },{
+      image_name: kannika_img,
+      full_name: "Kannika Angkurarat (May)",
+      position: "",
+    },
+    {
+      image_name: kannika_img,
+      full_name: "Kannika Angkurarat (May)",
+      position: "",
+    },
   ];
 
   const renderTeamCard = (item: TeamCardProps) => {
@@ -28,15 +75,22 @@ export default function TeamListSection() {
         <Image
           alt=""
           src={item.image_name}
-          className="h-full w-56 rounded-tr-3xl rounded-bl-3xl"
+          className="h-full w-full  rounded-tr-3xl rounded-bl-3xl"
         />
       </div>
+      // <div className="h-full w-56 rounded-tr-3xl rounded-bl-3xl overflow-hidden p-2 border border-primary">
+      //   <Image
+      //     alt=""
+      //     src={item.image_name}
+      //     className="h-full w-56 rounded-tr-3xl rounded-bl-3xl"
+      //   />
+      // </div>
     );
   };
 
   const renderCardList = () => {
     return (
-      <div className="m-4 h-full w-full flex gap-8">
+      <div className="h-full w-full flex justify-center flex-wrap gap-8  ">
         {data.map((item: TeamCardProps, index: number) => {
           return (
             <React.Fragment key={index}>{renderTeamCard(item)}</React.Fragment>
