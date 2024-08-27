98b064b6a75202d7e5e11f6abee5d9275670fd10
watcharapol <watcharapol.numpaya@gtnexttech.com>
2024-08-27 18:39:12 +0700

    [UPDATE] add info

--- src/app/our-service/sap/page.tsx
+++ src/app/our-service/sap/page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import SapMainBanner from "./components/sap-main-banner/sap-main-banner";
+import SapInfo from "./components/sap-info/sap-info";
 
 export default function SapPage() {
+
+
+
+
   const renderSap = () => {
     return (
       <div className="xl:container mx-auto">
         <SapMainBanner />
+        <div className="lg:px-0 px-2 py-4"><SapInfo/></div>
+         
       </div>
     );
   };
