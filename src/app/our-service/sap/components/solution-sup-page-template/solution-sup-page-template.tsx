5a2fa3d9d445d4bb624bf24e3b8caf783e2bb6a1
watcharapol <watcharapol.numpaya@gtnexttech.com>
2024-08-28 13:14:15 +0700

    [UPdATE] info popup

--- src/app/our-service/sap/components/solution-sup-page-template/solution-sup-page-template.tsx
+++ src/app/our-service/sap/components/solution-sup-page-template/solution-sup-page-template.tsx
@@ -5,7 +5,7 @@ export default function SolutionSupPagetemplate() {
     const renderInformation = () => {
         return (
           <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="relative w-5/6 h-full p-6 bg-white rounded-lg shadow-lg animate-slide-in">
+            <div className="relative w-5/6 h-5/6 p-6 bg-white rounded-lg shadow-lg animate-slide-in">
           
               <div className="text-center text-lg font-semibold text-gray-800">
                 Your content here
