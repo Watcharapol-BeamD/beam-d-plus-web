ceaeec7a92899426e37e78d9e96441050e48b63e
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2024-08-29 13:08:50 +0700

    [UPDATE magento finish

--- src/app/our-service/magento/page.tsx
+++ src/app/our-service/magento/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import MagentoMainBanner from "./components/magento-main-banner/magento-main-banner";
+import ContentSection from "./components/content-section/content-section";
 
 export default function MagentoPage() {
   const renderMagento = () => {
     return (
       <div className="xl:container mx-auto">
         <MagentoMainBanner />
+        <div className="px-2 my-4">  <ContentSection/></div>
+      
       </div>
     );
   };
