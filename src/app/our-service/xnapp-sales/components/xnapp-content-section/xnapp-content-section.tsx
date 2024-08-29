dfe5df96889e2b731934d858772c78b742ca325a
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2024-08-29 14:00:38 +0700

    [update] add image

--- src/app/our-service/xnapp-sales/components/xnapp-content-section/xnapp-content-section.tsx
+++ src/app/our-service/xnapp-sales/components/xnapp-content-section/xnapp-content-section.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
 import React from "react";
 import Xnapp_img from "@/app/assets/our-service/xnapp/digital-increasing-bar-graph-with-businessman-hand-overlay.jpg";
+import still_img from "@/app/assets/our-service/xnapp/still-life-fashion-designer-s-office.jpg";
+import data_img from "@/app/assets/our-service/xnapp/data.jpg";
+import route_img from "@/app/assets/our-service/xnapp/route.jpg";
+import monitor_img from "@/app/assets/our-service/xnapp/monitor.jpg";
+import color_coded_img from "@/app/assets/our-service/xnapp/color-coded-reports.jpg";
+import promo_img from "@/app/assets/our-service/xnapp/promo.jpg";
+import race_img from "@/app/assets/our-service/xnapp/race.jpg";
+import roi_img from "@/app/assets/our-service/xnapp/roi.jpg";
+import collaboration_img from "@/app/assets/our-service/xnapp/collaboration.jpg";
+
 
 export default function XnappContentSection() {
   const renderVision = () => {
@@ -35,11 +45,48 @@ export default function XnappContentSection() {
               systematic processes ensuring that products are never out of stock
               by replenishing demand at outlet shelves to distributor purchase.
             </p>
-          </div>{" "}
+          </div>
           <div className="md:w-1/2 w-full">
             <Image alt="" src={Xnapp_img} className="" />
           </div>
         </div>
+        {/* ----------------------------------------------- */}
+        <div className="flex md:flex-row-reverse flex-col-reverse">
+          <div className="md:w-1/2 w-full">
+            <p className="uppercase font-bold text-lg">
+              Brand + Variant + Size
+            </p>
+            <p className="text-base">
+              XnappSales ensures that the right brand, the right variant and the
+              right size of a product is available on the right shelf.
+              XnappSales uses research proven methodologies to drive penetration
+              by ensuring the Right Brand + Right Variant + Right Size is
+              available in the Right Places.
+            </p>
+          </div>
+          <div className="md:w-1/2 w-full">
+            <Image alt="" src={still_img} className="" />
+          </div>
+        </div>
+        {/* ----------------------------------------------- */}
+        <div className="flex md:flex-row-reverse flex-col-reverse">
+          <div className="md:w-1/2 w-full">
+            <p className="uppercase font-bold text-lg">
+              Brand + Variant + Size
+            </p>
+            <p className="text-base">
+              XnappSales ensures that the right brand, the right variant and the
+              right size of a product is available on the right shelf.
+              XnappSales uses research proven methodologies to drive penetration
+              by ensuring the Right Brand + Right Variant + Right Size is
+              available in the Right Places.
+            </p>
+          </div>
+          <div className="md:w-1/2 w-full">
+            <Image alt="" src={still_img} className="" />
+          </div>
+        </div>
+        {/* ----------------------------------------------- */}
       </div>
     );
   };
