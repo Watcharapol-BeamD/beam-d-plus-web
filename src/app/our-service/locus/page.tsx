c1ad11f308f6faa817529b633d32e0202737a0c3
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2024-08-29 09:57:30 +0700

    [UPDATE] main banner locus

--- src/app/our-service/locus/page.tsx
+++ src/app/our-service/locus/page.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import LocusMainBanner from './components/locus-main-banner'
+import React from "react";
+import LocusMainBanner from "./components/locus-main-banner";
 
 export default function LocusPage() {
+  const renderLocus = () => {
+    return (
+      <div className="xl:container mx-auto">
+        <LocusMainBanner />{" "}
+      </div>
+    );
+  };
 
-
-  return (
-    <div className="flex-1"><LocusMainBanner/> </div>
-  )
+  return <div className="flex-1">{renderLocus()}</div>;
 }
