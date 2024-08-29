6d429e0f24facf8fa1513cf09e06c0566b097a56
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2024-08-29 09:42:48 +0700

    [UPDATE] create locus

---
+++ src/app/our-service/locus/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LocusMainBanner from './components/locus-main-banner'
+
+export default function LocusPage() {
+
+
+  return (
+    <div className="flex-1"><LocusMainBanner/> </div>
+  )
+}
