fa7398ed967245741e8955c190070c9c02587cce
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2024-09-05 11:16:04 +0700

    [UPDATE] adjust image

--- src/app/homepage/components/partner-carousel/PartnerCarousel.tsx
+++ src/app/homepage/components/partner-carousel/PartnerCarousel.tsx
@@ -98,7 +98,7 @@ export default function PartnerCarousel() {
 
   const renderCarousel = () => {
     return (
-      <div className="h-96" id="partner-carousel">
+      <div className="h-96 p-1" id="partner-carousel"  >
         <Swiper
           // slidesPerView={5}
           grid={{
@@ -187,7 +187,7 @@ export default function PartnerCarousel() {
           // breakpoints={breakpoints}
         >
           {data.map((item, index) => (
-            <SwiperSlide key={index}>
+            <SwiperSlide key={index} className="">
               <div className="md:h-28 h-20 md:w-40 w-36">
                 <Image
                   alt=""
