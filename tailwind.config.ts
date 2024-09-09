2e8f8807de23f04e80ac1c3b298ddf92c870e82d
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2024-09-09 11:43:45 +0700

    [UPDATE] add slide animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,18 +16,28 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      animation: {
-        "slide-in-right": "slide-to-right 0.5s ease-out forwards",
+      animation: {   
         "slide-in-left": "slide-to-left 0.5s ease-out forwards",
+        "slide-in-right": "slide-to-right 0.5s ease-out forwards",
+        "slide-up": "slide-to-up 0.5s ease-out forwards",
+        "slide-down": "slide-to-down 0.5s ease-out forwards",
       },
-      keyframes: {
+      keyframes: {     
+        "slide-to-left": {
+          "0%": { transform: "translateX(100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
         "slide-to-right": {
           "0%": { transform: "translateX(-100%)", opacity: "0" },
           "100%": { transform: "translateX(0)", opacity: "1" },
         },
-        "slide-to-left": {
-          "0%": { transform: "translateX(100%)", opacity: "0" },
-          "100%": { transform: "translateX(0)", opacity: "1" },
+        "slide-to-up": {
+          "0%": { transform: "translateY(100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        "slide-to-down": {
+          "0%": { transform: "translateY(-100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
         },
       },
 
