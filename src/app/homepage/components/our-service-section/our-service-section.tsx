2e4ad552e6575d02022ea1f754303d8581e4aed7
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2024-09-09 13:34:49 +0700

    [UPDATE] add animation OUR SERVICES

--- src/app/homepage/components/our-service-section/our-service-section.tsx
+++ src/app/homepage/components/our-service-section/our-service-section.tsx
@@ -1,17 +1,21 @@
+"use client";
+
 import Button from "@/app/components/Button";
 import Image from "next/image";
 import React from "react";
 import ButtonLink from "@/app/components/ButtonLink";
 
- 
 import ml_img from "@/app/assets/our-service/ml.svg";
 import data_management_img from "@/app/assets/our-service/data-management.svg";
 import mobile_dev_img from "@/app/assets/our-service/mobile-development.svg";
 import silhouette_img from "@/app/assets/our-service/multiple-users-silhouette.svg";
 import thinking_img from "@/app/assets/our-service/thinking.svg";
 import development_img from "@/app/assets/our-service/web-development.svg";
+import { useVisibilityOnScroll } from "@/app/hooks/useVisibilityOnScroll";
 
 export default function OurServiceSection() {
+  const isVisible = useVisibilityOnScroll("our-service");
+
   const items = [
     {
       image: ml_img,
@@ -41,29 +45,28 @@ export default function OurServiceSection() {
 
   const renderOurService = () => {
     return (
-      <div id="our-service" className=" h-218 md:h-160 lg:h-128 relative bg-two_tone">
+      <div
+        id="our-service"
+        className="h-218 md:h-160 lg:h-128 relative bg-two_tone"
+      >
         <div className="absolute p-2 inset-0 py-4 md:py-6 grid grid-rows-[auto,1fr,auto] items-center justify-items-center">
           <p className="text-3xl font-bold text-white">OUR SERVICES</p>
 
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6  w-full justify-items-center items-center gap-2">
-            {items.map((item,index) => {
+            {items.map((item, index) => {
               return (
-                <div key={index} className="h-60 w-full max-w-44 bg-white rounded-xl p-4 shadow border-gray-200 border">
-                  <Image
-                    alt="icon"
-                    src={item.image}
-                    height={45}
-                    className=""
-                  />
+                <div
+                  key={index}
+                  className={`h-60 w-full max-w-44 bg-white rounded-xl p-4 shadow border-gray-200 border ${
+                    isVisible ?  `animate-fade-up animate-duration-500`  : ""
+                  }`}
+                >
+            
+                  <Image alt="icon" src={item.image} height={45} className="" />
                   <p className="mt-2">{item.description}</p>
                 </div>
               );
             })}
-            {/* <div className="h-60 w-40 bg-white rounded-xl"> </div>
-            <div className="h-60 w-40 bg-white rounded-xl"> </div>
-            <div className="h-60 w-40 bg-white rounded-xl"> </div>
-            <div className="h-60 w-40 bg-white rounded-xl"> </div>
-            <div className="h-60 w-40 bg-white rounded-xl"> </div> */}
           </div>
           <ButtonLink title="View All Service" url="/our-service" />
         </div>
