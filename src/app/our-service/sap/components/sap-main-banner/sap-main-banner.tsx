44247374e88354078bc445165db81845fb81f55f
watcharapol <watcharapol.numpaya@gtnexttech.com>
2024-09-10 11:55:51 +0700

    [UPDATE] issue 19

--- src/app/our-service/sap/components/sap-main-banner/sap-main-banner.tsx
+++ src/app/our-service/sap/components/sap-main-banner/sap-main-banner.tsx
@@ -11,14 +11,14 @@ export default function SapMainBanner() {
       <div className="relative bg-gray-300 bg-opacity-50 h-full w-full flex justify-center items-center">
         <div className="absolute bg-white bg-opacity-70 md:h-60 h-auto md:w-160 w-auto md:bottom-36 md:left-10 md:mx-0 mx-2 p-4 rounded-3xl space-y-4">
           <p className="md:text-4xl text-3xl font-bold">
-            SAP ERP and SAP B1 - Enterprise Resource Planning
+          SAP S/4 Hana and SAP B1 - Enterprise Resource Planning
           </p>
           <p className="text-lg ">
             Solutions for Enterprise Resource Planning from SAP
           </p>
-          <div>
+          {/* <div>
             <ButtonLink title="LEARN MORE" url="#" />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
