963f86bc2796743ef82126ab8ed951ba9df7820b
Watcharapol-BeamD <watcharapol.numpaya@gtnexttech.com>
2024-09-11 14:11:47 +0700

    [UPDATE] add email function

--- src/app/homepage/components/contact-us-form/ContactUsForm.tsx
+++ src/app/homepage/components/contact-us-form/ContactUsForm.tsx
@@ -1,49 +1,121 @@
-"use client"
+"use client";
 import Button from "@/app/components/Button";
-import { useVisibilityOnScroll } from "@/app/hooks/useVisibilityOnScroll";
-import React from "react";
+import emailjs, { EmailJSResponseStatus } from "@emailjs/browser";
+import React, { FormEvent, useRef, useState } from "react";
+
+import { Loading3QuartersOutlined } from "@ant-design/icons";
 
 export default function ContactUsForm() {
-  const isVisible = useVisibilityOnScroll("contact-us-section")
+  const [isSended, setIsSended] = useState<boolean>(false);
+  const form = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+    const serviceId = "service_q9s9gjr";
+    const templateId = "template_n97d793";
+    const publicId = "xNN5z4-4CSl_OIbqs";
+
+    e.preventDefault();
+
+    setIsSended(true);
+    if (form.current) {
+      emailjs
+        .sendForm(serviceId, templateId, form.current, {
+          publicKey: publicId,
+        })
+        .then(
+          (response: EmailJSResponseStatus) => {
+            console.log("SUCCESS!", response.status, response.text);
+            setIsSended(false);
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
+    }
+  };
 
   const renderContactForm = () => {
     return (
-      <div id="contact-us-section" className={`border py-6 shadow-md bg-white w-full ${isVisible?"animate-fade-up":""}`}>
+      <div id="contact-us" className="border py-6 shadow-md bg-white w-full">
         <p className="text-center text-3xl">LET'S CONNECT</p>
 
-        <div className="flex justify-center flex-col items-center mt-6">
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="flex justify-center flex-col items-center mt-6"
+        >
           <div className="grid sm:grid-cols-2 grid-cols-1 gap-8">
             <div>
               <p className=" ">First Name</p>
-              <input className="py-2 outline-none border-b-2 border-black " />
+              <input
+                type="text"
+                name="first_name"
+                className="py-2 outline-none border-b-2 border-black "
+              />
             </div>
+
             <div>
               <p className=" ">Last Name</p>
-              <input className="py-2 outline-none border-b-2 border-black " />
+              <input
+                type="text"
+                name="last_name"
+                className="py-2 outline-none border-b-2 border-black "
+              />
             </div>
+
             <div>
-              <p className=" ">Company Name</p>
-              <input className="py-2 outline-none border-b-2 border-black " />
+              <p className="">Company Name</p>
+              <input
+                type="text"
+                name="company_name"
+                className="py-2 outline-none border-b-2 border-black "
+              />
             </div>
+
             <div>
               <p className=" ">Company Email</p>
-              <input className="py-2 outline-none border-b-2 border-black " />
+              <input
+                type="email"
+                name="company_email"
+                className="py-2 outline-none border-b-2 border-black "
+              />
             </div>
+
             <div className="grid grid-col-1 gap-10">
               <div>
                 <p className=" ">Phone Number</p>
-                <input className="py-2 outline-none border-b-2 border-black " />
+                <input
+                  type="text"
+                  name="user_phone_number"
+                  className="py-2 outline-none border-b-2 border-black "
+                />
               </div>
+
               <div>
                 <p className=" ">Leave A Message</p>
-                <input className="py-2 outline-none border-b-2 border-black " />
+                <input
+                  type="text"
+                  name="message"
+                  className="py-2 outline-none border-b-2 border-black "
+                />
               </div>
             </div>
           </div>
           <div className="mt-4">
-            <Button title={"SENT MESSAGE"} />
+            <button
+              disabled={isSended}
+              type="submit"
+              className="bg-primary py-3 px-6 rounded-full text-white text-lg hover:scale-105 duration-300 flex item-center"
+            >
+              {isSended && (
+                <div className="animate-spin mr-3">
+                  <Loading3QuartersOutlined />
+                </div>
+              )}
+              SENT MESSAGE
+            </button>
           </div>
-        </div>
+        </form>
       </div>
     );
   };
