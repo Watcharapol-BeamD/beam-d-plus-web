fd203ad9140a8045c9d131853faa5099f72e1023
watcharapol <watcharapol.numpaya@gtnexttech.com>
2024-09-13 16:16:17 +0700

    [UPDATE] prevent animate bug

--- src/app/our-service/sap/page.tsx
+++ src/app/our-service/sap/page.tsx
@@ -1,12 +1,15 @@
+"use client"
 import React from "react";
 import SapMainBanner from "./components/sap-main-banner/sap-main-banner";
 import SapInfoSection from "./components/sap-info/sap-info-section";
 import ResourcePlanningSection from "./components/resource-planning-section/resource-planning-section";
+import { useVisibilityOnScroll } from "@/app/hooks/useVisibilityOnScroll";
 
 export default function SapPage() {
+  const isVisible = useVisibilityOnScroll("sap-page")
   const renderSap = () => {
     return (
-      <div className="xl:container mx-auto">
+      <div id="sap-page" className="xl:container mx-auto">
         <SapMainBanner />
         <div className=" md:px-10 px-2 py-4">
           <SapInfoSection />
@@ -16,5 +19,7 @@ export default function SapPage() {
     );
   };
 
-  return <div className="flex-1 animate-fade">{renderSap()}</div>;
+  return <div className={`flex-1 ${isVisible?" ":"animate-fade"} ` }>{renderSap()}</div>;
 }
+//need to use this ${isVisible?"":"animate-fade"} 
+//animate-fade conflict wiih read more popup,navbar will hidden screen
